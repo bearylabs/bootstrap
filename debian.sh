fe9a1688c135e205f6412e3bf092543f58e1ac85
bearylabs <66212473+bearylabs@users.noreply.github.com>
2025-03-14 09:27:32 +0100

    add github cli

--- debian.sh
+++ debian.sh
@@ -6,6 +6,18 @@ then
     sudo apt-get install git -y
 fi
 
+# Check if GitHub CLI is already installed; if not, add its repository to Apt and install it
+if ! command -v gh 2>&1 >/dev/null
+then
+    (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+        && sudo mkdir -p -m 755 /etc/apt/keyrings \
+            && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+            && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+        && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+        && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+        && sudo apt update \
+        && sudo apt install gh -y
+fi
 
 # Check if Tmux is already installed; if not, download git repo and install it
 if ! command -v tmux 2>&1 >/dev/null
